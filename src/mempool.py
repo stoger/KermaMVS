042b9a8509e197b5522fc5098df4b870f30527ad
Maximilian Schenk <e11908528@student.tuwien.ac.at>
2023-12-15 20:29:08 +0100

    Code Together 3

--- src/mempool.py
+++ src/mempool.py
@@ -30,36 +30,47 @@ def get_all_txids_in_blocks(blocks):
     pass  # TODO
 
 
-def get_all_blocks_from_chain(chaintip):
+def get_all_blocks_from_chain(chaintip, end_block):
     tip = objects.get_object(chaintip)
     blocks = list()
     next_block = objects.get_object(tip['previd'])
     blocks.append(objects.get_objid(next_block))
-    while next_block is not const.GENESIS_BLOCK_ID:
+    while next_block is not end_block:
         next_block = objects.get_object(next_block['previd'])
         blocks.append(objects.get_objid(next_block))
     return blocks[::-1]
 
 
 def get_common_ancestor(old_chaintip, new_chaintip):
-    old_blocks = get_all_blocks_from_chain(old_chaintip)
-    new_blocks = get_all_blocks_from_chain(new_chaintip)
+    old_blocks = get_all_blocks_from_chain(old_chaintip, const.GENESIS_BLOCK_ID)
+    new_blocks = get_all_blocks_from_chain(new_chaintip, const.GENESIS_BLOCK_ID)
     greatest_index = 0
     for block in old_blocks:
         if block in new_blocks and old_blocks.index(block) > greatest_index:
             greatest_index = old_blocks.index(block)
 
-    return objects.get_object(old_blocks[greatest_index]) if greatest_index != 0 else objects.get_object(const.GENESIS_BLOCK_ID)
+    return objects.get_object(old_blocks[greatest_index]) if greatest_index != 0 else objects.get_object(
+        const.GENESIS_BLOCK_ID)
 
 
 # get (id of lca, list of old blocks from lca, list of new blocks from lca)
 def get_lca_and_intermediate_blocks(old_tip: str, new_tip: str):
-    pass  # TODO
+    ancestor = get_common_ancestor(old_tip, new_tip)
+    old_blocks = get_all_blocks_from_chain(old_tip, ancestor)
+    new_blocks = get_all_blocks_from_chain(new_tip, ancestor)
+
+    return ancestor, old_blocks, new_blocks
 
 
 def rebase_mempool(old_tip, new_tip, mptxids):
     ancestor = get_common_ancestor(old_tip, new_tip)
     print(f"found ancestor: {ancestor}", flush=True)
+
+    # cur_block = objects.get_object(old_tip)
+    # oid = old_tip
+    # reapply_tx = []
+    # while cur_block != ancestor:
+    #     reapply_tx += objects.get_block_txs()
     pass  # TODO
 
 
@@ -82,6 +93,7 @@ class Mempool:
         return True  # TODO
 
     def rebase_to_block(self, bid: str):
+        rebase_mempool(self.base_block_id, bid, [])
         (block, utxo, _) = objects.get_block_utxo_height(bid)
 
         # gather all blocks between bid & latest state
